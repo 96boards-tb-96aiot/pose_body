4666cd40f84c7d6b88ea1cb4edeb5c3d7c0388b4
Zhihua Wang <hogan.wang@rock-chips.com>
2020-03-20 19:03:55 +0800

    support usb camera
    Signed-off-by: Zhihua Wang <hogan.wang@rock-chips.com>
    Change-Id: Ifc0f95306e9cbf686f1c95b481f8624f901a1b37

--- main.c
+++ main.c
@@ -44,20 +44,71 @@
 #include "shadow_display.h"
 #include "video_common.h"
 #include "rockx_control.h"
+#include "usb_camera.h"
+
+void usage(const char *name)
+{
+    printf("Usage: %s options\n", name);
+    printf("-h --help  Display this usage information.\n"
+           "-i --isp   Use isp camera.\n"
+           "-u --usb   Use usb camera.\n");
+    printf("e.g. %s -i\n", name);
+    printf("e.g. %s -u\n", name);
+    exit(0);
+}
 
 int MiniGUIMain(int argc, const char *argv[])
 {
+    int next_option;
+    bool isp_en = false;
+    bool usb_en = false;
+
+    const char* const short_options = "hiu";
+    const struct option long_options[] = {
+        {"help", 0, NULL, 'h'},
+        {"isp", 0, NULL, 'i'},
+        {"usb", 0, NULL, 'u'},
+    };
+
+    do {
+        next_option = getopt_long(argc, argv, short_options, long_options, NULL);
+        switch (next_option) {
+        case 'i':
+            isp_en = true;
+            break;
+        case 'u':
+            usb_en = true;
+            break;
+        case -1:
+            break;
+        default:
+            usage(argv[0]);
+            break;
+        }
+    } while (next_option != -1);
+
+    if (!isp_en && !usb_en)
+        usage(argv[0]);
+
     register_shadow_display_vertical(shadow_display_vertical);
 
     if (rockx_control_init())
         return -1;
 
-    if (rkisp_control_init())
-        return -1;
+    if (isp_en) {
+        if (rkisp_control_init())
+            return -1;
+    } else if (usb_en) {
+        if (usb_camera_init(1280, 720))
+            return -1;
+    }
 
     ui_run();
 
-    rkisp_control_exit();
+    if (isp_en)
+        rkisp_control_exit();
+    else if (usb_en)
+        usb_camera_exit();
 
     rockx_control_exit();
 
